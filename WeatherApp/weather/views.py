cf7a4122c66e6f52d94982325bd0ac03f063daa4
rostekrostek <rostekrostek@yandex.ru>
2021-12-05 14:39:20 +0300

    05.12.2021

---
+++ WeatherApp/weather/views.py
@@ -0,0 +1,34 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    appid = '9ecc3087817f2d76bf53eacc26d93286'
+    url= 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': res["main"]["temp"],
+            'icon': res["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+    context = {
+        'all_info': all_cities,
+        'form': form,
+    }
+
+    return render(request, 'weather/index.html', context)
